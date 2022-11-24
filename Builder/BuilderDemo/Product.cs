8523bfd0c299b766c910110e349dafbe19d61895
xSmair <simonmair01@gmx.at>
2022-11-24 08:43:30 +0100

    Added Builder

---
+++ Builder/BuilderDemo/Product.cs
@@ -0,0 +1,68 @@
+﻿namespace BuilderDemo;
+
+public class Product
+{
+    private string _requiredA = null!;
+    private string _requiredB = null!;
+    private int _optionalC;
+    private string? _optionalD;
+
+    public string RequiredA => _requiredA;
+    public string RequiredB => _requiredB;
+    public int OptionalC => _optionalC;
+    public string? OptionalD => _optionalD;
+    
+    
+    private Product(Builder builder)
+    {
+        _requiredA = builder._requiredA;
+        _requiredB = builder._requiredB;
+        _optionalC = builder._optionalC;
+        _optionalD = builder._optionalD;
+    }
+
+    public override string ToString()
+    {
+        return $"{_requiredA}, {_requiredB}, {_optionalC}, {_optionalD}";
+    }
+
+    public class Builder
+    {
+        internal string _requiredA = null!;
+        internal string _requiredB = null!;
+        internal int _optionalC;
+        internal string? _optionalD;
+        
+        public Builder(string requiredA, string requiredB)
+        {
+            _requiredA = requiredA;
+            _requiredB = requiredB;
+        }
+        
+        public Builder WithOptionalC(int optionalC)
+        {
+            _optionalC = optionalC;
+            return this;
+        }
+        
+        public Builder WithOptionalD(string? optionalD)
+        {
+            _optionalD = optionalD;
+            return this;
+        }
+        
+        public Product Build()
+        {
+            var product = new Product(this);
+            if(_optionalD== null)
+                throw new ArgumentNullException("OptionalD must be set if you want to use it");
+            if(_optionalC<0)
+                throw new ArgumentException("OptionalC must be greater than 0");
+            if (_optionalD.Equals(""))
+                throw new ArgumentException("If u want to set OptionalD, it must be not empty");
+            return product;
+        }
+    }
+    
+    
+}
---
+++ Builder/BuilderDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿using BuilderDemo;
+
+var product = new Product.Builder("RequiredA", "RequiredB")
+    .WithOptionalC(12)
+    .WithOptionalD("OptionalD")
+    .Build();
+Console.WriteLine(product);
+
