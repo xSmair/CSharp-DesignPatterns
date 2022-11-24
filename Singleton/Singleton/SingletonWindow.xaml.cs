8bd1531a56586d68fcff0bcf449cf07904045e0b
xSmair <simonmair01@gmx.at>
2022-11-24 09:05:10 +0100

    Added Singleton

---
+++ Singleton/Singleton/SingletonWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Singleton;
+
+public partial class SingletonWindow : Window
+{
+    private static SingletonWindow _instance;
+    
+    public static SingletonWindow Instance => _instance ??= new SingletonWindow();
+
+    public int RandomNumber { get; }
+    
+    private SingletonWindow()
+    {
+        InitializeComponent();
+        RandomNumber = new Random().Next(0, 100);
+    }
+
+    private void SingletonWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        RandomNumberLabel.Content = RandomNumber + "";
+    }
+}
